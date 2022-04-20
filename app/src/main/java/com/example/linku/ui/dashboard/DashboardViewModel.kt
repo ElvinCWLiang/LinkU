4da8b3cd4671db2d72fd117975544bb49d05daac
ElvinCWLiang <andyluong2006@gmail.com>
2022-04-20 22:23:37 +0800

    [LinkU]: Initial Project Update
    [Change]
    
    [Symptom]
    
    [Cause]
    Initial Update
    Uploaded from Original Google Navigation Bar Project
    
    [Solution]
    
    [Issue type]

---
+++ app/src/main/java/com/example/linku/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.linku.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+
+}
